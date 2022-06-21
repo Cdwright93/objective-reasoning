e1b8ed207803a183ae8c6a8706e7bd4e02ef3042
clayton wright <claytonwright@claytons-MacBook-Pro.local>
2022-06-21 14:24:26 -0400

    all working but last

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
-
-
+function isAdmin (user){
+    if(user.userRole == 'ADMIN'){
+        return true
+    }
+    if(user.userRole != 'ADMIN'){
+        return false
+    }
+}
+function getEmail (user){
+    return `${user.firstName.toLowerCase()}.${user.lastName.toLowerCase()}@codeimmersives.com`
+}
+function getPlaylistLength (playlist){
+    return playlist.songs.length
+    if(playlist.songs.length == undefined){
+        playlist.songs = []
+        return playlist.songs.length
+    }
+}
+function getHardestHomework (homeworkArr){
+    if (homeworkArr.length == 0){
+        return ''
+    }
+    if(homeworkArr.length > 0){
+        let hardestHomework = homeworkArr[0]
+        homeworkArr.forEach(assignment => {
+            if(assignment.averageScore < hardestHomework.averageScore){
+                hardestHomework = assignment
+            }
+        });
+        return hardestHomework.name
+    }
+}
+function createPhonebook(people,numbers){
+    let phonebook = []
+    let ctr = 0
+    people.forEach(person => {
+        phonebook.push(`${person}:${numbers[ctr]}`)
+        ctr++
+    })
+    console.log(phonebook)
+    return phonebook
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
